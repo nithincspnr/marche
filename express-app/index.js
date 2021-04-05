4095e7ffb42c8bea5a07ae691fca9878cd5d9673
nithincs <nithincspnr@gmail.com>
2021-04-05 09:32:39 +0530

    fix CORS issue

--- express-app/index.js
+++ express-app/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const port = 5000;
 
 const stateController = require('./State');
+
+app.use(cors());
 
 app.get('/', (req, res) => res.send('Hello world'));
 app.get('/states', stateController.getStates);
